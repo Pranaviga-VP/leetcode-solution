fad914b5ed1cb2c2afa99b6dc37bedaea5522bc5
Pranaviga-VP <pranavika20palanisamy@gmail.com>
2026-02-04 16:36:08 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,6 @@
+// Last updated: 2/4/2026, 4:35:57 PM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[] { celsius + 273.15, celsius * 1.80 + 32.00 };
+    }
+}
