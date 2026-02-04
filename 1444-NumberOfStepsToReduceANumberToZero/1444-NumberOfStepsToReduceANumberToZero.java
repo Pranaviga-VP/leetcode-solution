f354a30f49e8740804b919c66005045d014333cb
Pranaviga-VP <pranavika20palanisamy@gmail.com>
2026-02-04 16:36:46 +0530

    Create 1444-NumberOfStepsToReduceANumberToZero

---
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,15 @@
+// Last updated: 2/4/2026, 4:36:40 PM
+class Solution {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
