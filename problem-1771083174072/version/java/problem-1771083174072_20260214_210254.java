85e1727f91bb4480bb0bc56283a798d31a08ba9a
Pranaviga V P <pranavika20palanisamy@gmail.com>
2026-02-14 21:03:06 +0530

    Create

---
+++ problem-1771083174072/version/java/problem-1771083174072_20260214_210254.java
@@ -0,0 +1,17 @@
+// Last updated: 2/14/2026, 9:02:54 PM
+1class Solution {
+2    public String mapWordWeights(String[] words, int[] weight) {
+3    StringBuilder result = new StringBuilder();
+4        for(String word : words){
+5            int sum = 0;
+6            for(char ch : word.toCharArray()){
+7                sum += weight[ch - 'a'];
+8            }
+9            int remainder = sum%26;
+10            char mappedChar = (char)('z' - remainder);
+11            result.append(mappedChar);
+12        }
+13        return result.toString();
+14        
+15    }
+16}
